85769f061d9b7766265083bb12d6df9f7538ea93
j- <j@skeoh.com>
2016-06-04 16:29:39 +1000

    Add tests for base parameter

--- test/count-digits.js
+++ test/count-digits.js
@@ -98,3 +98,54 @@ test('Can count infinity', (t) => {
 test('Does not count NaN', (t) => {
 	t.truthy(isNaN(countDigits(NaN)));
 });
+
+test('Can count base 2 numbers', (t) => {
+	t.is(countDigits(0b1, 2), 1, '1');
+	t.is(countDigits(0b10, 2), 2, '10');
+	t.is(countDigits(0b11, 2), 2, '11');
+	t.is(countDigits(0b100, 2), 3, '100');
+	t.is(countDigits(0b111, 2), 3, '111');
+	t.is(countDigits(0b1000, 2), 4, '1000');
+	t.is(countDigits(0b1111, 2), 4, '1111');
+	t.is(countDigits(0b10000, 2), 5, '10000');
+	t.is(countDigits(0b11111, 2), 5, '11111');
+});
+
+test('Can count base 8 numbers', (t) => {
+	t.is(countDigits(0o1, 8), 1, '1');
+	t.is(countDigits(0o7, 8), 1, '7');
+	t.is(countDigits(0o10, 8), 2, '10');
+	t.is(countDigits(0o77, 8), 2, '77');
+	t.is(countDigits(0o100, 8), 3, '100');
+	t.is(countDigits(0o777, 8), 3, '777');
+	t.is(countDigits(0o1000, 8), 4, '1000');
+	t.is(countDigits(0o7777, 8), 4, '7777');
+	t.is(countDigits(0o10000, 8), 5, '10000');
+	t.is(countDigits(0o77777, 8), 5, '77777');
+});
+
+test('Can count base 10 numbers', (t) => {
+	t.is(countDigits(1, 10), 1, '1');
+	t.is(countDigits(9, 10), 1, '9');
+	t.is(countDigits(10, 10), 2, '10');
+	t.is(countDigits(99, 10), 2, '99');
+	t.is(countDigits(100, 10), 3, '100');
+	t.is(countDigits(999, 10), 3, '999');
+	t.is(countDigits(1000, 10), 4, '1000');
+	t.is(countDigits(9999, 10), 4, '9999');
+	t.is(countDigits(10000, 10), 5, '10000');
+	t.is(countDigits(99999, 10), 5, '99999');
+});
+
+test('Can count base 16 numbers', (t) => {
+	t.is(countDigits(0x1, 16), 1, '1');
+	t.is(countDigits(0xf, 16), 1, 'f');
+	t.is(countDigits(0x10, 16), 2, '10');
+	t.is(countDigits(0xff, 16), 2, 'ff');
+	t.is(countDigits(0x100, 16), 3, '100');
+	t.is(countDigits(0xfff, 16), 3, 'fff');
+	t.is(countDigits(0x1000, 16), 4, '1000');
+	t.is(countDigits(0xffff, 16), 4, 'ffff');
+	t.is(countDigits(0x10000, 16), 5, '10000');
+	t.is(countDigits(0xfffff, 16), 5, 'fffff');
+});
