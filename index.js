b4b0eea3d0b5753af7c44865a3d86555cf82c7e4
j- <j@skeoh.com>
2016-06-04 15:55:51 +1000

    Make module ES5-friendly

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const ceil = Math.ceil;
-const log = Math.log;
+var ceil = Math.ceil;
+var log = Math.log;
 
-module.exports = function countDigits (x, base) {
+function countDigits (x, base) {
 	return ceil(log(x + 1) / log(base || 10));
-};
+}
+
+module.exports = countDigits;
